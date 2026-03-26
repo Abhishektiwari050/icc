3469d51022a8cb68cf359f98864233f489228fdb
Antishake-Cell <antishake605@gmail.com>
2026-03-26 13:38:08 +0530

    UX Fix: Completely remove redundant FloatingNav from ClientWrapper and Navbar

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { FloatingNav } from "@/components/ui/floating-navbar";
-import { Home, Package, Search, Phone } from "lucide-react";
+import { useEffect } from "react";
 
-const navItems = [
-  { name: "Home", link: "/", icon: <Home className="w-4 h-4" /> },
-  { name: "Services", link: "/services", icon: <Package className="w-4 h-4" /> },
-  { name: "Tracking", link: "/tracking", icon: <Search className="w-4 h-4" /> },
-  { name: "Contact", link: "/contact", icon: <Phone className="w-4 h-4" /> },
-];
+
 
 import Lenis from "lenis";
 import { ThemeProvider } from "next-themes";
@@ -32,7 +25,6 @@ export const ClientWrapper = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-      <FloatingNav navItems={navItems} />
       {children}
     </ThemeProvider>
   );
